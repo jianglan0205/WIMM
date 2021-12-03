ef961c4130ef4f66c88ab1b4326e36a3613d0d80
jianglan <250455653@qq.com>
2021-12-03 15:23:34 +0800

    创建<Icon />

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg class="icon">
+    <use v-bind:xlink:href="'#'+name"/>
+  </svg>
+</template>
+
+<script lang="ts">
+
+// eslint-disable-next-line no-undef
+let importAll = (requireContext:__WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+}catch(error){
+  console.log(error);
+}
+//将svg统一全部引入
+
+export default {
+  props:['name'],
+  name: 'Icon'
+};
+</script>
+
+<style lang="scss" scoped>
+.icon{
+    width: 1em; height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack:config =>{
+    const dir = path.resolve(__dirname,'src/assets/icons')
+
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)  //所有svg走这个规则
+      .include.add(dir).end()  //设置只包含 icons 目录的svg走这个规则
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+      //设置需要使用的loader,并且不要把它解析出文件来
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+      //设置需要使用的插件
+    config.module.rule('svg').exclude.add(dir)
+      //其他svg loader 排除 icon 目录(这个目录里的svg已经走过规则)
+
+  }
 }
