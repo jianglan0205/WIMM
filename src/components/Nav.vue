82e7a5638b957eff9ab71ebd272221a1243c46a5
jianglan <250455653@qq.com>
2021-12-09 21:45:55 +0800

    添加全剧样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -23,6 +23,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
   nav{
     display: flex;
     box-shadow: 0 0 3px rgba(0,0,0,0.5);
@@ -41,7 +43,7 @@ export default {
       }
     }
     > .selected{
-      color: red;
+      color: $color-highlight;
     }
 }
 
