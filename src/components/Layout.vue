1d7395eed261306a15058508b491d869265f03bc
jianglan <250455653@qq.com>
2021-12-10 23:27:57 +0800

    完成HTML和CSS

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
       <slot/>
     </div>
-    <Nav />
+    <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 export default {
+  props: ['classPrefix'],
   name: 'Layout'
 };
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
+.layout-wrapper {
 
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
-.content{
+
+.content {
   overflow: auto;
-  flex-grow:1;
+  flex-grow: 1;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -26,8 +26,8 @@ export default {
   @import "~@/assets/style/helper.scss";
 
   nav{
+    @extend %outerShadow;
     display: flex;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
     flex-direction: row;
     font-size: 12px;
     > .item{
