2ccfd3d3bae646b5d3873d4fbe2861bb894fe799
jianglan <250455653@qq.com>
2022-03-10 15:50:51 +0800

    将 tag 的 string 改为 ｛id,name｝

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,34 +4,39 @@ import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
+import EditLabel from "@/views/EditLabel.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    redirect: '/money'
-  },
-  {
-    path: '/money',
-    component: Money
-  },
-  {
-    path: '/labels',
-    component: Labels
-  },
-  {
-    path: '/statistics',
-    component: Statistics
-  },
-  {
-    path: '*',
-    component: NotFound
-  }
+    {
+        path: '/',
+        redirect: '/money'
+    },
+    {
+        path: '/money',
+        component: Money
+    },
+    {
+        path: '/labels',
+        component: Labels
+    },
+    {
+        path: '/statistics',
+        component: Statistics
+    },
+    {
+        path: '/labels/edit',
+        component: EditLabel
+    },
+    {
+        path: '*',
+        component: NotFound
+    }
 ];
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 export default router;
